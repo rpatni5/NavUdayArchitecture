c4bae4c2f945a52b4861690ec810dfe30eb83cab
amitaKunwar <amitakunwar99@gmail.com>
2023-10-25 14:29:28 +0530

    removed classes

--- NavUdayArchitecture/ClientApp/src/app/component/user-dashboard/user-dashboard.component.ts
+++ NavUdayArchitecture/ClientApp/src/app/component/user-dashboard/user-dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavMenuComponent } from '../nav-menu/nav-menu.component';
 import { FrameworkService } from './framework-service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { distinctUntilChanged } from 'rxjs/internal/operators/distinctUntilChanged';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -9,10 +12,35 @@ import { FrameworkService } from './framework-service';
 })
 export class UserDashboardComponent implements OnInit {
   globalActiveMenuItem: any = null;
+  Breakpoints = Breakpoints;
+  currentBreakpoint:string = '';
+  isLinear = false;
+  readonly breakpoint$ = this.breakpointObserver
+  .observe([Breakpoints.Large, Breakpoints.Medium, Breakpoints.Small, '(min-width: 500px)'])
+  .pipe(
+    tap(value => console.log(value)),
+    distinctUntilChanged()
+  );
 
-  constructor(private frameworkService: FrameworkService) {}
+  constructor(private frameworkService: FrameworkService, private breakpointObserver: BreakpointObserver) {}
 
+
+ 
+  private breakpointChanged() {
+    if(this.breakpointObserver.isMatched(Breakpoints.Large)) {
+      this.currentBreakpoint = Breakpoints.Large;
+    } else if(this.breakpointObserver.isMatched(Breakpoints.Medium)) {
+      this.currentBreakpoint = Breakpoints.Medium;
+    } else if(this.breakpointObserver.isMatched(Breakpoints.Small)) {
+      this.currentBreakpoint = Breakpoints.Small;
+    } else if(this.breakpointObserver.isMatched('(min-width: 500px)')) {
+      this.currentBreakpoint = '(min-width: 500px)';
+    }
+  }
   ngOnInit(): void {
+    this.breakpoint$.subscribe(() =>
+    this.breakpointChanged()
+  );
     this.frameworkService.activeMenu.subscribe((menu) => {
       this.globalActiveMenuItem = menu;
     });
